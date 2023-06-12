134aa0c5b347a4d90738f44770b0758ab5b7dfff
BerkaySimsek1 <berkay.simsek.098@gmail.com>
2023-06-12 14:26:53 +0300

    Initial Commit

---
+++ SpaceX Launch Tracker/SpaceX_Launch_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceX_Launch_TrackerApp.swift
+//  SpaceX Launch Tracker
+//
+//  Created by Berkay on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpaceX_Launch_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
